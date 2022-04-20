81be9d8fbe63f7e9e7953be9c9721d0ed42045f4
cT9all7 <102795309+cT9all7@users.noreply.github.com>
2022-04-20 20:26:06 +0300

    Update parent.c

--- parent.c
+++ parent.c
@@ -42,7 +42,6 @@ int main(int argc, char* argv[])
 	struct msqid_ds* buf;
 	int ret,ret1,stat,stat1;
 	char a;
-	flag=true;
 	int i=0;
 	int i1=0;
 	for(int T=0;;T++)
@@ -51,48 +50,47 @@ int main(int argc, char* argv[])
 		if(a=='p')//+producer
 		{
 			pid=fork();
-			if(pid>0){ mpid[i]=pid; i++;}
+			if(pid>0){ 
+				mpid[i]=pid; 
+				i++;
+			}
 			if(!pid) ret=execve("/home/kali/progi/lab02/child",NULL,NULL);
-			printf("\n%d\n",i);
+			printf("\nproducer %d\n",i);
 		}
 		else if(a=='P')//+consumer
 		{
 			pid1=fork();
-			if(pid1>0){ mpid1[i1]=pid1; i1++;}
-			printf("\n%d\n",i1);
+			if(pid1>0){ 
+				mpid1[i1]=pid1;
+				i1++;
+			}
 			if(!pid1) ret1=execve("/home/kali/progi/lab02/child1",NULL,NULL);
+			printf("\nconsumer %d\n",i1);
 		}
-		else if(a=='-')
+		else if(a=='d')
 		{
-			printf("kill processes\n");
+			semctl(semid,0,IPC_RMID);	
+			msgctl(msqid,IPC_RMID,buf);
+			printf("\nkilled msg queue\n");
+		}
+		else if(a=='q') 
+		{
+			printf("\n kill producers %d\n",i);
 			for(int j=0;j<i;j++)
 			{
 				kill(mpid[j],SIGKILL);
 				waitpid(pid,&stat,0);
 			}
-			for(int j=0;j<i1;j++){
+			i=0;
+		}
+		else if(a=='Q')
+		{
+			printf("\n kill consumers %d\n\n",i1);
+			for(int j=0;j<i1;j++)
+			{
 				kill(mpid1[j],SIGKILL);
 				waitpid(pid1,&stat1,0);
 			}
-			i=0;
-			i1=0;
-		}
-		else if(a=='q') 
-		{
-			semctl(semid,0,IPC_RMID);
-			msgctl(msqid,IPC_RMID,buf);
-			printf("buy buy\n");
-			for(int j=0;j<i;j++)
-			{
-				kill(mpid[j],SIGINT);
-				waitpid(pid,&stat,0);
-			}
-			for(int j=0;j<i1;j++)
-			{
-				kill(mpid1[j],SIGINT);
-				waitpid(pid1,&stat1,0);
-			}
-			i=0;
 			i1=0;
 			printf("buy buy\n");
 		}
